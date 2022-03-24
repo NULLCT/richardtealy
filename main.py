261725acb699b8c685073236eecde1d644649a1c
NULLCT <cockatiel.u10@gmail.com>
2022-03-24 17:31:58 +0900

    owari

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!python3
 
 import requests
+import os
 from bs4 import BeautifulSoup
 
+RESULTFOLDER = "out"
 
 def printLogo():
     print('''
@@ -14,13 +16,25 @@ def printLogo():
                                                     |___/
             ''')
 
+def cutUntilSlash(s: str) -> str:
+  res = ""
+  for i in range(len(s)-1,0,-1):
+    if s[i] == "/":
+      break
+    else:
+      res = s[i] + res
+
+  return res
 
 def parseURL(url: str):
     response = requests.get(url)
     print(response)
     soup = BeautifulSoup(response.text, "html.parser")
+    res = []
     for imgtag in soup.find_all("img", class_="media-wrapper"):
       print("https:" + imgtag["src"])
+      res.append("https:" + imgtag["src"])
+    return res
 
 
 def main():
@@ -29,7 +43,16 @@ def main():
     while True:
         print("URL? > ", end="")
         url = input()
-        parseURL(url)
+        images = parseURL(url)
+
+        if len(images) == 0:
+          print("ERROR: any images not found")
+        else:
+          os.makedirs(RESULTFOLDER+"/"+cutUntilSlash(url))
+          for image in images:
+            response = requests.get(image).content
+            with open(RESULTFOLDER+"/"+cutUntilSlash(url)+"/"+cutUntilSlash(image)+".png", "wb") as f:
+              f.write(response)
 
 
 if __name__ == "__main__":
